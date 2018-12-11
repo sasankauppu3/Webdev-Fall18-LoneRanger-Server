b6f37186b51839909edd9e2c4743bc016828b581
Veera Venkata Sasanka Uppu <uppu.v@husky.neu.edu>
2018-12-10 20:15:44 -0500

    deploy

--- data/db.js
+++ data/db.js
@@ -1,7 +1,7 @@
 module.exports = function () {
     const mongoose = require('mongoose');
 
-    var connectionString = 'mongodb://127.0.0.1:27017/foodie';
+    var connectionString = 'mongodb://root:root1234@ds229878.mlab.com:29878/heroku_07fwmpth';
     mongoose.connect(connectionString);
 
 }
